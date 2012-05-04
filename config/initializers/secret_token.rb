315012db3eb8ca455a8b4f6750ab325c132fccd4
py890000 <py890000@gmail.com>
2012-05-04 15:22:19 +0800

    fff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hi::Application.config.secret_token = '969f0abd26c26a71b204784ef32668c07046b25ae48820485175acb413739c9d923b0b4abdb5577352c55ccb2bf1f35ede7d347ea0e7ee9382120a65452ce481'
