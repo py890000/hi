9527a4499cda72a4e726da221924eeb11537c3f2
py890000 <py890000@gmail.com>
2012-05-14 10:31:18 +0800

    fuck

--- app/models/demo.rb
+++
@@ -1,5 +0,0 @@
-class Demo < ActiveRecord::Base
-  attr_accessible :login_name, :login_password, :userInfo
-  
-  validates_presence_of :login_name
-end
--- app/models/user_info.rb
+++
@@ -1,3 +0,0 @@
-class UserInfo < ActiveRecord::Base
-  attr_accessible :age, :creatTime, :email, :loginer, :password, :updateTime
-end
